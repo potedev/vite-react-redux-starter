d06e1656602d7dcfbe9c6d3605d304ffe1164c83
Quentin Danneville <quentindanneville@MacBook-Pro-de-Quentin.local>
2021-11-06 20:02:21 +0100

    [Redux] Toolkit Added

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,9 +1,14 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './store/counter'
+import logo from '../assets/logo.svg'
+import '../css/App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
+  // const [count, setCount] = useState(0)
 
   return (
     <div className="App">
@@ -11,9 +16,23 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          {/* <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
+          </button> */}
+          <button
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            Increment
           </button>
+          <span>{count}</span>
+          <button
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            Decrement
+          </button>
+
         </p>
         <p>
           Edit <code>App.jsx</code> and save to test HMR updates.
