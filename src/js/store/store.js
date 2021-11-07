991c8cba126449094eeb2b5e235026322001d57e
Quentin Danneville <quentindanneville@MacBook-Pro-de-Quentin.local>
2021-11-07 17:52:23 +0100

    [Redux] Logger added

--- src/js/store/store.js
+++ src/js/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './counter'
+import logger from 'redux-logger'
 
 export const store = configureStore({
     reducer: {
         counter: counterReducer
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 })
